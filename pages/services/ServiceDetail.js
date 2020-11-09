309a3086f7268a5ab3eccd664a0c90d0c2faedd8
Anita RASOA <anita.ras@onja.org>
2020-11-09 10:05:18 +0300

    Learning react router

--- pages/services/ServiceDetail.js
+++ pages/services/ServiceDetail.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
+import serviceData from './serviceData';
 
 function ServiceDetail(props) {
-	return <h1>Service Detail Page</h1>;
+	const { serviceId } = useParams();
+	const history = useHistory();
+	const location = useLocation();
+	console.log(location);
+	const thisService = serviceData.find(service => service._id === serviceId);
+
+	function handleClick() {
+		console.log("Submitting...");
+		setTimeout(() => {
+			history.push("/services")
+		}, 2000);
+	}
+
+	return (
+		<div>
+			<h1>Service Detail Page</h1>
+			<h3>{thisService.name} - ${thisService.price}</h3>
+			<p>{thisService.description}</p>
+			<button onClick={handleClick}>Go to somewhere</button>
+		</div>
+	)
 }
 
 export default ServiceDetail;
--- pages/services/ServicesList.js
+++ pages/services/ServicesList.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import serviceData from './serviceData';
+import {Link} from 'react-router-dom';
 
 function ServicesList() {
 	return (
 		<div>
 			<h1>Services List Page</h1>
+			{serviceData.map(service => (
+			<div key={service._id}>
+				<Link to={`/services/${service._id}`}><h2>{service.name} - ${service.price}</h2></Link>
+					<p>{service.description}</p>
+				</div>
+			))}
 		</div>
 	);
 }
